9acdf792d4ff2701bbb480de7d2160f3524c6133
capdaseletni-sys <capdaseletni@gmail.com>
2025-12-21 21:22:30 +0800

    Update ppv.py

--- ppv.py
+++ ppv.py
@@ -3,93 +3,30 @@ from playwright.async_api import async_playwright
 import aiohttp
 from datetime import datetime
 
-API_URL = "https://ppv.to/api/streams"
+API_URL = "https://istreameast.app/nbastreams2"
 
 CUSTOM_HEADERS = [
-    '#EXTVLCOPT:http-origin=https://ppv.to',
-    '#EXTVLCOPT:http-referrer=https://ppv.to/',
+    '#EXTVLCOPT:http-origin=https://gooz.aapmains.net',
+    '#EXTVLCOPT:http-referrer=https://gooz.aapmains.net/',
     '#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:143.0) Gecko/20100101 Firefox/143.0'
 ]
 
 ALLOWED_CATEGORIES = {
-    "24/7 Streams", "Wrestling", "Football", "Basketball", "Baseball",
-    "Combat Sports", "American Football", "Darts", "Motorsports", "Ice Hockey"
+   "Basketball"
 }
 
 CATEGORY_LOGOS = {
-    "24/7 Streams": "http://drewlive24.duckdns.org:9000/Logos/247.png",
-    "Wrestling": "http://drewlive24.duckdns.org:9000/Logos/Wrestling.png",
-    "Football": "http://drewlive24.duckdns.org:9000/Logos/Football.png",
-    "Basketball": "http://drewlive24.duckdns.org:9000/Logos/Basketball.png",
-    "Baseball": "http://drewlive24.duckdns.org:9000/Logos/Baseball.png",
-    "American Football": "http://drewlive24.duckdns.org:9000/Logos/NFL3.png",
-    "Combat Sports": "http://drewlive24.duckdns.org:9000/Logos/CombatSports2.png",
-    "Darts": "http://drewlive24.duckdns.org:9000/Logos/Darts.png",
-    "Motorsports": "http://drewlive24.duckdns.org:9000/Logos/Motorsports2.png",
-    "Live Now": "http://drewlive24.duckdns.org:9000/Logos/DrewLiveSports.png",
-    "Ice Hockey": "http://drewlive24.duckdns.org:9000/Logos/Hockey.png"
+    "Basketball": "http://drewlive24.duckdns.org:9000/Logos/Basketball.png"
 }
 
 CATEGORY_TVG_IDS = {
-    "24/7 Streams": "24.7.Dummy.us",
-    "Wrestling": "PPV.EVENTS.Dummy.us",
-    "Football": "Soccer.Dummy.us",
-    "Basketball": "Basketball.Dummy.us",
-    "Baseball": "MLB.Baseball.Dummy.us",
-    "American Football": "NFL.Dummy.us",
-    "Combat Sports": "PPV.EVENTS.Dummy.us",
-    "Darts": "Darts.Dummy.us",
-    "Motorsports": "Racing.Dummy.us",
-    "Live Now": "24.7.Dummy.us",
-    "Ice Hockey": "NHL.Hockey.Dummy.us"
+    "Basketball": "Basketball.Dummy.us"
 }
 
 GROUP_RENAME_MAP = {
-    "24/7 Streams": "PPVLand - Live Channels 24/7",
-    "Wrestling": "PPVLand - Wrestling Events",
-    "Football": "PPVLand - Global Football Streams",
-    "Basketball": "PPVLand - Basketball Hub",
-    "Baseball": "PPVLand - MLB",
-    "American Football": "PPVLand - NFL Action",
-    "Combat Sports": "PPVLand - Combat Sports",
-    "Darts": "PPVLand - Darts",
-    "Motorsports": "PPVLand - Racing Action",
-    "Live Now": "PPVLand - Live Now",
-    "Ice Hockey": "PPVLand - NHL Action"
+    "Basketball": "Live Sports"
 }
 
-NFL_TEAMS = {
-    "arizona cardinals", "atlanta falcons", "baltimore ravens", "buffalo bills",
-    "carolina panthers", "chicago bears", "cincinnati bengals", "cleveland browns",
-    "dallas cowboys", "denver broncos", "detroit lions", "green bay packers",
-    "houston texans", "indianapolis colts", "jacksonville jaguars", "kansas city chiefs",
-    "las vegas raiders", "los angeles chargers", "los angeles rams", "miami dolphins",
-    "minnesota vikings", "new england patriots", "new orleans saints", "new york giants",
-    "new york jets", "philadelphia eagles", "pittsburgh steelers", "san francisco 49ers",
-    "seattle seahawks", "tampa bay buccaneers", "tennessee titans", "washington commanders"
-}
-
-COLLEGE_TEAMS = {
-    "alabama crimson tide", "auburn tigers", "arkansas razorbacks", "georgia bulldogs",
-    "florida gators", "lsu tigers", "ole miss rebels", "mississippi state bulldogs",
-    "tennessee volunteers", "texas longhorns", "oklahoma sooners", "oklahoma state cowboys",
-    "baylor bears", "tcu horned frogs", "kansas jayhawks", "kansas state wildcats",
-    "iowa state cyclones", "iowa hawkeyes", "michigan wolverines", "ohio state buckeyes",
-    "penn state nittany lions", "michigan state spartans", "wisconsin badgers",
-    "minnesota golden gophers", "illinois fighting illini", "northwestern wildcats",
-    "indiana hoosiers", "notre dame fighting irish", "usc trojans", "ucla bruins",
-    "oregon ducks", "oregon state beavers", "washington huskies", "washington state cougars",
-    "arizona wildcats", "stanford cardinal", "california golden bears", "colorado buffaloes",
-    "florida state seminoles", "miami hurricanes", "clemson tigers", "north carolina tar heels",
-    "duke blue devils", "nc state wolfpack", "wake forest demon deacons", "syracuse orange",
-    "virginia cavaliers", "virginia tech hokies", "louisville cardinals", "pittsburgh panthers",
-    "maryland terrapins", "rutgers scarlet knights", "nebraska cornhuskers", "purdue boilermakers",
-    "texas a&m aggies", "kentucky wildcats", "missouri tigers", "vanderbilt commodores",
-    "houston cougars", "utah utes", "byu cougars", "boise state broncos", "san diego state aztecs",
-    "cincinnati bearcats", "memphis tigers", "ucf knights", "south florida bulls", "smu mustangs",
-    "tulsa golden hurricane", "tulane green wave", "navy midshipmen", "army black knights",
-    "arizona state sun devils", "texas tech red raiders", "florida atlantic owls"
-}
 
 # --- CORRECTED FUNCTION #1 ---
 async def check_m3u8_url(url, referer):
