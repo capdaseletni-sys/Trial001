fce56aa733b1b669cc95ab0c80ef3ab0fd659d60
capdaseletni-sys <capdaseletni@gmail.com>
2026-01-16 13:16:40 +0800

    Update update_playlists.sh

--- update_playlists.sh
+++ update_playlists.sh
@@ -2,8 +2,11 @@
 
 # Define the output files and their corresponding URLs
 urls=(
-    "http://supersonictv.live:8080/get.php?username=611366&password=682256&&type=m3u_plus"
-    "http://www.sansat.plus:88/get.php?username=02060789178359&password=02:00:00:00:00:00&type=m3u"   
+    "http://supersonictv.live:8080/get.php?username=611366&password=682256&&type=m3u_plus",
+    "http://www.sansat.plus:88/get.php?username=02060789178359&password=02:00:00:00:00:00&type=m3u",
+    "http://cord-cutter.net:8080/get.php?username=15564292&password=15564292&type=m3u_plus",
+    "http://protv65.shop:8080/get.php?username=hsn3868&password=hsn00xy&type=m3u_plus",
+    "http://portal-iptv.net:8080/get.php?username=077998950140&password=077998950140&&type=m3u_plus"
 )
 
 # Array to keep track of downloaded filenames
